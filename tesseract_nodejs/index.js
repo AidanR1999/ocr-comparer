8477d4446a6e280847bd63b5e463b13b6a492eca
Aidan Rooney <aidanr1999@hotmail.com>
2021-02-17 16:51:16 +0000

    log results to file

--- tesseract_nodejs/index.js
+++ tesseract_nodejs/index.js
@@ -1,21 +1,37 @@
+//libraries
 const ts = require ('tesseract.js');
 const fs = require('fs');
 
+//absolute directory for images and results
 const unalteredDir = "E:/repos/ocr_comparer/tests/unaltered/"
-const extension = ".tif"
+const resultsDir = "E:/repos/ocr_comparer/results/tesseract/"
 
+//read all image filenames
 var unaltered = fs.readdirSync(unalteredDir);
 
+//run function loop 
 readImg(unalteredDir + unaltered[0], 0);
 
+//runs engine, params: img path, iteration
 function readImg(img, i) {
     try {
+        //run ocr engine params: img path, language, logging function
         ts.recognize(
             img,
             'eng',
+            //log progress to console
             { logger: m=> console.log(m.progress) }
+        //run on completion
         ).then(({data: {text}})=>{
-            console.log('done');
+            console.log('done' + img);
+
+            //log results to file params: path, text, callback
+            fs.writeFile(resultsDir + unaltered[i] + '.txt', text, (err) => {
+                if (err) throw err;
+                console.log('logged');
+            });
+
+            //loop
             readImg(unalteredDir + unaltered[i + 1], i + 1);
     })
     }
